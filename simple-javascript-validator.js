8b3fc9896f158c3e06e2e088230a0482ad2b8e7d
kapitan <kapitannwel@gmail.com>
2020-07-02 20:29:58 +0800

    renamed node_modules folder to verndor

--- simple-javascript-validator.js
+++ simple-javascript-validator.js
@@ -151,7 +151,7 @@ function includeFile(file_name, defer_value, time_delay, file_type) {
   setTimeout(function(){ head.appendChild(file); }, time_delay);
 }
 
-includeFile('./node_modules/jquery/dist/jquery.min.js', false, 1, 'script');
-includeFile('./node_modules/toastr/build/toastr.min.js', true, 50, 'script');
-includeFile('./node_modules/toastr/build/toastr.min.css', false, 100, 'link');
+includeFile('./vendor/jquery/dist/jquery.min.js', false, 1, 'script');
+includeFile('./vendor/toastr/build/toastr.min.js', true, 50, 'script');
+includeFile('./vendor/toastr/build/toastr.min.css', false, 100, 'link');
 /*  end - include external files  */
